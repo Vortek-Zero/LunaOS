f0b4e2a60ba55ae350c78ca1817cb4d036414984
Vortek <luna.os2732@gmail.com>
2026-09-10 23:26:27 -0300

    M1: Create adapters package

---
+++ agent/adapters/__init__.py
@@ -0,0 +1,5 @@
+"""Adapters for integrating with legacy systems."""
+
+from agent.adapters.legacy_tools import LegacyToolAdapter, LegacyToolsAdapter
+
+__all__ = ["LegacyToolAdapter", "LegacyToolsAdapter"]
